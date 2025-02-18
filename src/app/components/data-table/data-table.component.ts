236406020d2ed29df80cca8b8b25c01e5c081f38
Harsh Shah Jaiswal <tech.harsh2003@gmail.com>
2025-02-18 16:00:11 +0530

    pagination implemented

--- src/app/components/data-table/data-table.component.ts
+++ src/app/components/data-table/data-table.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { User } from '../../models/user.model';
 import { DataService } from '../../services/data.service';
 
-import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import {MatPaginator, MatPaginatorModule, PageEvent} from '@angular/material/paginator';
 import {MatTableDataSource, MatTableModule} from '@angular/material/table';
 
 @Component({
@@ -23,9 +23,43 @@ export class DataTableComponent implements OnInit {
 
   ngOnInit(): void {
     this.dataService.getUsers().subscribe((data) =>{ 
-      // this.users = data
-      this.dataSource.data = data
+      this.users = data
+      this.length = data.length
+      this.dataSource.data = this.users.slice(this.startIndex, this.pageSize);
   });
   }
+  shownlist:any;
+  
+    length:number = 0;
+    pageSize:number = 5;
+    startIndex:number=0;
+    endindex:number=0;
+    pageIndex:number = 0;
+    pageSizeOptions:number[] = [5, 10, 25];
+  
+    hidePageSize = false;
+    showPageSizeOptions = true;
+    showFirstLastButtons = true;
+    disabled = false;
+  
+    pageEvent!: PageEvent;
+  
+    handlePageEvent(e: PageEvent) {
+      this.pageEvent = e;
+      this.length = e.length;
+      this.startIndex=e.pageIndex*e.pageSize;
+      this.endindex=e.pageIndex*e.pageSize+e.pageSize;
+      this.pageSize = e.pageSize;
+      this.pageIndex = e.pageIndex;
+      this.dataSource.data = this.users.slice(this.startIndex, this.endindex);
+      
+    }
+  
+    setPageSizeOptions(setPageSizeOptionsInput: string) {
+      if (setPageSizeOptionsInput) {
+        this.pageSizeOptions = setPageSizeOptionsInput.split(',').map(str => +str);
+      }
+    }
+
 }
 
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -15,6 +15,14 @@ export class DataService {
     { id: 6, name: 'Alice Johnson', email: 'alice@example.com' },
     { id: 7, name: 'Jane Smith', email: 'jane@example.com' },
     { id: 8, name: 'Alice Johnson', email: 'alice@example.com' },
+    { id: 9, name: 'John Doe', email: 'john@example.com' },
+    { id: 10, name: 'Jane Smith', email: 'jane@example.com' },
+    { id: 11, name: 'Alice Johnson', email: 'alice@example.com' },
+    { id: 12, name: 'John Doe', email: 'john@example.com' },
+    { id: 13, name: 'Jane Smith', email: 'jane@example.com' },
+    { id: 14, name: 'Alice Johnson', email: 'alice@example.com' },
+    { id: 15, name: 'Jane Smith', email: 'jane@example.com' },
+    { id: 16, name: 'Alice Johnson', email: 'alice@example.com' },
   ];
 
   getUsers(): Observable<User[]> {
