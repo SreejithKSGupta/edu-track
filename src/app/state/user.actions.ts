4ececc6d595659966c153ec3fc274b4d58664c18
Harsh Shah Jaiswal <tech.harsh2003@gmail.com>
2025-02-19 14:46:56 +0530

    lazy loading

--- src/app/state/user.actions.ts
+++ src/app/state/user.actions.ts
@@ -1,7 +1,30 @@
 import { createAction, props } from '@ngrx/store';
 import { User } from '../models/user.model';
 
-export const loadUsers = createAction('[User] Load Users');
-export const loadUsersSuccess = createAction('[User] Load Users Success', props<{ users: User[] }>());
-export const loadUsersFailure = createAction('[User] Load Users Failure', props<{ error: string }>());
-export const setPagination = createAction('[User] Set Pagination',props<{ pageIndex: number; pageSize: number }>());
+export const loadUsers = createAction('[User] Load Initial Users');
+export const loadUsersSuccess = createAction(
+  '[User] Load Users Success',
+  props<{ users: User[] }>()
+);
+export const loadUsersFailure = createAction(
+  '[User] Load Users Failure',
+  props<{ error: any }>()
+);
+
+export const loadMoreUsers = createAction(
+    '[User] Load More Users',
+    props<{ users: User[] }>() 
+  );
+export const loadMoreUsersSuccess = createAction(
+  '[User] Load More Users Success',
+  props<{ users: User[] }>()
+);
+export const loadMoreUsersFailure = createAction(
+  '[User] Load More Users Failure',
+  props<{ error: any }>()
+);
+
+export const setPagination = createAction(
+  '[User] Set Pagination',
+  props<{ pageIndex: number; pageSize: number }>()
+);
