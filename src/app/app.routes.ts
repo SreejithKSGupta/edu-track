eaa09acbe87ba43fd064f8c7110452a47b912be7
sreejith ks <sreejithksgupta2255@gmail.com>
2025-02-19 14:41:29 +0530

    created dashboard and sign in forms

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { SigninComponent } from './pages/signin/signin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+     { path: 'dashboard' , component:DashboardComponent },
+     {path:'signin',component:SigninComponent}
+];
