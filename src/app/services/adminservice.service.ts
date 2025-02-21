f9d0cb374a183e07a598c259dfde6550e68efea6
sreejith ks <sreejithksgupta2255@gmail.com>
2025-02-21 16:28:23 +0530

    signin flow corrected

--- src/app/services/adminservice.service.ts
+++ src/app/services/adminservice.service.ts
@@ -13,21 +13,15 @@ export class AdminserviceService {
   constructor(private http: HttpClient){}
 
   addUser(user:any):Observable<any>{
-    const username = user.username;
-    const name = user.username;
-    const password = user.password;
-    const body = { name, username, password };
-    console.log(body);
-    debugger
-    return this.http.post('http://localhost:5000/create-user',body);
+    return this.http.post('http://localhost:5000/create-user',user);
 
   }
 
   checksignin(user:any):Observable<any>{
-    const username = user.username;
-    const password = user.password;
-    const body = { username, password };
-    console.log(body);
-    return this.http.post('http://localhost:5000/users', body);
+    // const username = user.username;
+    // const password = user.password;
+    // const body = { username, password };
+    // console.log(body);
+    return this.http.post('http://localhost:5000/users', user);
   }
 }
