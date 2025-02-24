adda51694e17645b869430d52334c11bb45d280f
sreejith ks <sreejithksgupta2255@gmail.com>
2025-02-24 17:33:57 +0530

    added a psudo notifications panel

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
 import { ThemeService } from '../../services/theme.service';
 import { Router } from '@angular/router';
+import { NotificationpanelComponent } from '../notificationpanel/notificationpanel.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  // imports: [ NotificationpanelComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
   isDarkMode: boolean = false;
 
-  constructor(private themeService: ThemeService,private router: Router) {}
+  constructor(private themeService: ThemeService,private router: Router,private dialog: MatDialog) {}
 
   ngOnInit() {
 
@@ -25,6 +27,17 @@ export class HeaderComponent {
 
   openitem(link: string) {
     this.router.navigate([`/${link}`]);
+  }
+
+  openNotifications(){
+      console.log('Opening Notifications');
+      this.dialog.open(NotificationpanelComponent, {
+        position: { right: '5vw', top: '10vh' },
+        width: '400px',
+        height: '500px',
+        data: { name: 'Notification Panel' }
+      });
+
   }
 
 }
