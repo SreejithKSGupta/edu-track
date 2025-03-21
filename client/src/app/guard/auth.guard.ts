7ffa829cba06b8018cfacd818174ac7c8c01eaed
sreejith ks <sreejithksgupta2255@gmail.com>
2025-03-21 14:12:31 +0530

    added an icon

--- client/src/app/guard/auth.guard.ts
+++ client/src/app/guard/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, Router } from '@angular/router';
+import {  CanActivate, CanActivateChild, Router } from '@angular/router';
 import { AuthserviceService } from '../services/authservice.service';
 import { BehaviorSubject } from 'rxjs';
 
@@ -7,38 +7,30 @@ import { BehaviorSubject } from 'rxjs';
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate, CanActivateChild {
-  // Track loading state
   public isLoading$ = new BehaviorSubject<boolean>(false);
 
   constructor(private router: Router, private authService: AuthserviceService) {}
 
-  canActivate(route: ActivatedRouteSnapshot): Promise<boolean> {
-    // Return a Promise to handle the authentication check asynchronously
-    console.log(route)
+  canActivate(): Promise<boolean> {
     return new Promise((resolve) => {
-      // Start loading
       this.isLoading$.next(true);
 
-      // Add a small delay to simulate network request time
       setTimeout(() => {
-        // Check authentication
         const isAuthenticated = this.authService.isAuthenticated();
 
         if (!isAuthenticated) {
-          // Navigate to signin page if not authenticated
           this.router.navigate(['/signin'], { replaceUrl: true });
         }
 
-        // Stop loading
         this.isLoading$.next(false);
 
-        // Resolve with authentication result
+
         resolve(isAuthenticated);
-      }, 500); // Adjust timing as needed
+      }, 200);
     });
   }
 
-  canActivateChild(route: ActivatedRouteSnapshot): Promise<boolean> {
-    return this.canActivate(route);
+  canActivateChild(): Promise<boolean> {
+    return this.canActivate();
   }
 }
