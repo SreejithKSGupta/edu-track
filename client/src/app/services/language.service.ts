a152fb7ce201f7c77d51ab8bfd2df2ad45c76be0
Harsh Jaiswal <tech.harsh2003@gmail.com>
2025-03-25 12:09:47 +0530

    google translate working

--- client/src/app/services/language.service.ts
+++ client/src/app/services/language.service.ts
@@ -7,20 +7,28 @@ import { TranslateService } from '@ngx-translate/core';
 export class LanguageService {
 
   constructor(private translate: TranslateService) {
-    this.translate.setDefaultLang('en');
+      this.translate.setDefaultLang(this.defaultLanguage());
+    
+  }
+
+  defaultLanguage(): string{
+    let language: string = 'fr';
+    if (typeof window !== 'undefined' && window.localStorage) {
+      language = localStorage.getItem('Language') || 'fr';
+    }
+    return language;
   }
 
   switchLanguage(language: string): void {
+    localStorage.setItem("Language", language);
     this.translate.use(language);
   }
 
   getCurrentLanguage(): string {
-    console.log(this.translate.currentLang);
-    
     return this.translate.currentLang;
   }
 
   initializeLanguage(): void {
-    this.translate.use('en');
+    this.translate.use(this.defaultLanguage());
   }
 }
